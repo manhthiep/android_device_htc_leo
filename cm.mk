324c80581794da5590f97642ef978d0b940578da
Arif Ali <mail@arif-ali.co.uk>
2011-12-03 22:35:09 +0000

    leo: a few changes

--- cm.mk
+++ cm.mk
@@ -11,9 +11,11 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/htc/leo/leo.mk)
 
 ## Device identifier. This must come after all inclusions
+PRODUCT_MODEL := HTC HD2
+PRODUCT_MANUFACTURER := HTC
 PRODUCT_BRAND := htc
 PRODUCT_DEVICE := leo
 PRODUCT_NAME := cm_leo
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=leo BUILD_ID=ITL41D BUILD_DISPLAY_ID=ITL41D BUILD_FINGERPRINT="google/full_leo/leo:4.0.1/ITL41D/223971:user/release-keys" PRVIATE_BUILD_DESC="full_leo-user 4.0.1 ITL41D 223971 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=leo BUILD_ID=ITL41D BUILD_DISPLAY_ID=ITL41D BUILD_FINGERPRINT="google/htc_leo/leo:4.0.1/ITL41D/223971:user/release-keys" PRVIATE_BUILD_DESC="full_leo-user 4.0.1 ITL41D 223971 release-keys"
