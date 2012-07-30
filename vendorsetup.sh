027ce0bd52bb921ca4e5a8364077acd1fd537970
Manh Thiep <manhthiep@gmail.com>
2012-07-30 01:57:37 -0700

    add ev_leo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-#add_lunch_combo full_leo-userdebug
-add_lunch_combo htc_leo-eng
+add_lunch_combo htc_leo-userdebug
+add_lunch_combo ev_leo-userdebug
